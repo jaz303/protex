f336640823efdf61863e78d1fcc8f1097f333dbd
Jason Frame <jason@onehackoranother.com>
2014-07-12 21:43:01 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -2,55 +2,55 @@ module.exports = protex;
 
 function protex() {
 
-	var locked = false;
+    var locked = false;
 
-	function isPromise(thing) {
-		return thing instanceof Promise;
-	}
+    function isPromise(thing) {
+        return thing instanceof Promise;
+    }
 
-	function isLocked() {
-		return locked;
-	}
+    function isLocked() {
+        return locked;
+    }
 
-	function execPromise(promise) {
-		locked = true;
-		return promise.then(function() {
-			locked = false;
-		});
-	}
+    function execPromise(promise) {
+        locked = true;
+        return promise.then(function() {
+            locked = false;
+        });
+    }
 
-	function execFunction(fn) {
-		locked = true;
-		var result = fn();
-		if (isPromise(result)) {
-			return execPromise(result);
-		} else {
-			locked = false;
-			return result;
-		};
-	}
+    function execFunction(fn) {
+        locked = true;
+        var result = fn();
+        if (isPromise(result)) {
+            return execPromise(result);
+        } else {
+            locked = false;
+            return result;
+        };
+    }
 
-	function exec(thing) {
+    function exec(thing) {
 
-		if (locked) {
-			throw new Error("protex is already locked!");
-		}
+        if (locked) {
+            throw new Error("protex is already locked!");
+        }
 
-		locked = true;
+        locked = true;
 
-		if (typeof thing === 'function') {
-			return execFunction(thing);
-		} else if (thing instanceof Promise) {
-			return execPromise(thing);
-		} else {
-			throw new Error("exec() expects either a function or a promise");
-		}
+        if (typeof thing === 'function') {
+            return execFunction(thing);
+        } else if (thing instanceof Promise) {
+            return execPromise(thing);
+        } else {
+            throw new Error("exec() expects either a function or a promise");
+        }
 
-	}
+    }
 
-	return {
-		isLocked	: function() { return locked; },
-		exec 		: exec
-	};
+    return {
+        isLocked    : function() { return locked; },
+        exec        : exec
+    };
 
 }
--- test/all.js
+++ test/all.js
@@ -4,64 +4,64 @@ global.Promise = require('es6-promise').Promise;
 
 test("locking with promises", function(assert) {
 
-	assert.plan(4);
+    assert.plan(4);
 
-	function makeLockCheck(shouldBeLocked) {
-		return function() {
-			assert.equal(p.isLocked(), shouldBeLocked);
-			return new Promise(function(resolve, reject) {
-				process.nextTick(resolve);
-			});
-		}
-	}
+    function makeLockCheck(shouldBeLocked) {
+        return function() {
+            assert.equal(p.isLocked(), shouldBeLocked);
+            return new Promise(function(resolve, reject) {
+                process.nextTick(resolve);
+            });
+        }
+    }
 
-	var p = protex();
+    var p = protex();
 
-	var result = p.exec(
-		Promise
-			.resolve()
-			.then(makeLockCheck(true))
-			.then(makeLockCheck(true))
-			.then(makeLockCheck(true))
-	)
+    var result = p.exec(
+        Promise
+            .resolve()
+            .then(makeLockCheck(true))
+            .then(makeLockCheck(true))
+            .then(makeLockCheck(true))
+    )
 
-	result.then(makeLockCheck(false));
+    result.then(makeLockCheck(false));
 
 });
 
 test("locking with functions", function(assert) {
 
-	var p = protex();
+    var p = protex();
 
-	var result = p.exec(function() {
-		assert.equal(p.isLocked(), true, "should be locked whilst function is executing");
-		return 10;
-	});
+    var result = p.exec(function() {
+        assert.equal(p.isLocked(), true, "should be locked whilst function is executing");
+        return 10;
+    });
 
-	assert.equal(p.isLocked(), false, "should not be locked after function call");
-	assert.equal(result, 10, "return value should be return value of function");
-	assert.end();
+    assert.equal(p.isLocked(), false, "should not be locked after function call");
+    assert.equal(result, 10, "return value should be return value of function");
+    assert.end();
 
 });
 
 test("function returning promise", function(assert) {
 
-	assert.plan(4);
+    assert.plan(4);
 
-	var p = protex();
+    var p = protex();
 
-	var result = p.exec(function() {
-		assert.ok(p.isLocked(), "should be locked in body of function");
-		return new Promise(function(resolve, reject) {
-			assert.ok(p.isLocked(), "should be locked in body of promise");
-			process.nextTick(resolve);
-		});
-	});
+    var result = p.exec(function() {
+        assert.ok(p.isLocked(), "should be locked in body of function");
+        return new Promise(function(resolve, reject) {
+            assert.ok(p.isLocked(), "should be locked in body of promise");
+            process.nextTick(resolve);
+        });
+    });
 
-	assert.ok(p.isLocked(), "should be locked after function returns");
+    assert.ok(p.isLocked(), "should be locked after function returns");
 
-	result.then(function() {
-		assert.ok(!p.isLocked(), "should be unlocked once promise is complete");
-	});
+    result.then(function() {
+        assert.ok(!p.isLocked(), "should be unlocked once promise is complete");
+    });
 
 });
